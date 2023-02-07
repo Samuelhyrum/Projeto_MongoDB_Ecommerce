c33abe42a0f603a1927dd809eedfcba34fddd056
Samuel Hyrum <samuelhymp@gmail.com>
2023-02-07 16:39:23 -0300

    Requisito 11, Retornando o nome do produto, a quantidade de curtidas e quantos itens foram vendidos dos produtos que não sejam iguais a Big Mac e McChicken

---
+++ challenges/desafio11.js
@@ -0,0 +1,12 @@
+db.produtos.find({
+    $and: [
+      { nome: { $ne: "Big Mac" } },
+       { nome: { $ne: "McChicken" } },
+    ],
+  
+  }, {
+    nome: 1,
+    curtidas: 1,
+    vendidos: 1,
+    _id: 0,
+  });
