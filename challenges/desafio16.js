ef40ba0a379673c803e00ad574432c7d9fcdefe5
Samuel Hyrum <samuelhymp@gmail.com>
2023-02-07 17:43:17 -0300

    Requisito 16, Adicionando o campo ultimaModificacao com a data corrente somente no sanduíche Big Mac

---
+++ challenges/desafio16.js
@@ -0,0 +1,10 @@
+db.produtos.updateMany(
+    { nome: "Big Mac" },
+    {
+    $currentDate: { ultimaModificacao: { $type: "timestamp" } },
+  },
+);
+db.produtos.find({ ultimaModificacao: { $exists: true } }, {
+    nome: 1,
+    _id: 0,
+});
